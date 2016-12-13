c43f36b0d2e4df9fd64385df16c7ed393114c3d0
o-yuto <onishi_yuto@yse-c.net>
2016-12-13 16:12:40 +0900

    ShowText.java

---
+++ ShowText.java
@@ -0,0 +1,7 @@
+public class ShowText{
+	public static void main( String[] args){
+		FileReader fr = null;
+		BufferedReader in = null;
+
+		fr = new fileReader(deta.txt);
+		in = new BufferedReader(fr);
